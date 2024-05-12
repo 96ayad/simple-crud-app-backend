c09c287ace9808bedc8609ccbcbe10bcd56cd956
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-12 19:30:12 +0300

    ok

--- routes/liquidity.route.js
+++ routes/liquidity.route.js
@@ -3,9 +3,8 @@ const Liquidity = require("../models/liquidity.model.js");
 const router = express.Router();
 const {getLiquidities, getLiquiditys, getLiquidity, createLiquidity, updateLiquidity, deleteLiquidity} = require('../controllers/liquidity.controller.js');
 
-
+////////////ayad//////////
 router.get("/:basemint/:quotemint", getLiquidities);
-
 router.get('/', getLiquiditys);
 router.get("/:id", getLiquidity);
 
