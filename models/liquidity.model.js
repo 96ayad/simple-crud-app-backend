e0f968bdfccbf6380a0964a7a938b010b7b9a3c4
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-12 20:02:30 +0300

    model

--- models/liquidity.model.js
+++ models/liquidity.model.js
@@ -8,11 +8,6 @@ const LiquiditySchema = mongoose.Schema(
       required: [true, "Please enter Liquidity id"],
     },
 
-    authority: {
-      type: String,
-      required: true,
-    },
-
     baseMint: {
       type: String,
       required: true,
@@ -21,7 +16,122 @@ const LiquiditySchema = mongoose.Schema(
     quoteMint: {
         type: String,
         required: true,
-      },
+    },
+
+    lpMint: {
+        type: String,
+        required: true,
+    },
+
+    baseDecimals: {
+        type: Number,
+        required: true,
+    },
+
+    quoteDecimals: {
+        type: Number,
+        required: true,
+    },
+
+    lpDecimals: {
+        type: Number,
+        required: true,
+    },
+
+    version: {
+        type: Number,
+        required: true,
+        default: 4,
+    },
+
+    programId: {
+        type: String,
+        required: true,
+        default: "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8",
+    },
+
+    authority: {
+        type: String,
+        required: true,
+    },
+
+    openOrders: {
+        type: String,
+        required: true,
+    },
+
+    targetOrders: {
+        type: String,
+        required: true,
+    },
+
+    baseVault: {
+        type: String,
+        required: true,
+    },
+
+    quoteVault: {
+        type: String,
+        required: true,
+    },
+
+    withdrawQueue: {
+        type: String,
+        required: true,
+    },
+
+    lpVault: {
+        type: String,
+        required: true,
+    },
+
+    marketVersion: {
+        type: Number,
+        required: true,
+        default: 4,
+    },
+
+    marketProgramId: {
+        type: String,
+        required: true,
+        default: "srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX",
+    },
+
+    marketId: {
+        type: String,
+        required: true,
+    },
+
+    marketAuthority: {
+        type: String,
+        required: true,
+    },
+
+    marketBaseVault: {
+        type: String,
+        required: true,
+    },
+
+    marketQuoteVault: {
+        type: String,
+        required: true,
+    },
+
+    marketBids: {
+        type: String,
+        required: true,
+    },
+
+    marketAsks: {
+        type: String,
+        required: true,
+    },
+
+    marketEventQueue: {
+        type: String,
+        required: true,
+    },
+      
 
   },
   {
